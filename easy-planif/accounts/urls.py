fbfa36062709d7e439a79e07b0049a2e5f350655
Luis Domingues <domingues.luis@protonmail.ch>
2025-02-22 16:05:20 +0100

    WIP: Add user management

--- easy-planif/accounts/urls.py
+++ easy-planif/accounts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('users/', views.UsersView.as_view(), name="users"),
+]
