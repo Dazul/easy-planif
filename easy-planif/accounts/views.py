0ad682cfe5034697508b44b16d23d5e852310376
Luis Domingues <domingues.luis@protonmail.ch>
2025-02-22 16:05:20 +0100

    Add user management

--- easy-planif/accounts/views.py
+++ easy-planif/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils.safestring import mark_safe
 
+from .models import *
 # Create your views here.
+
+class UsersView(generic.ListView):
+    model = CustomUser
+    template_name = 'users.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = ''
+        for m in CustomUser.objects.all():
+            d += f'<li> {m.username} </li>'
+        context['users'] = mark_safe(d)
+        return context
