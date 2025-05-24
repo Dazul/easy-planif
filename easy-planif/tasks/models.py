8e90b1fa3152b1ad355c6141aca984e237540ca7
Luis Domingues <domingues.luis@protonmail.ch>
2025-05-24 10:47:09 +0200

    WIP: Add authorizations management

--- easy-planif/tasks/models.py
+++ easy-planif/tasks/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from accounts.models import CustomUser
 
 class Tasks(models.Model):
     task_name = models.CharField(max_length=50)
+
+class Authorizations(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
