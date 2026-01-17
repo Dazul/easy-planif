f27735a99a6ff034db8e265dfef7f3e96c386369
Luis Domingues <Dazul@users.noreply.github.com>
2026-01-17 15:10:25 +0100

    Add some logic validation on Events (#23)
    * Add some logic validation on Events
    
    This commit adds validation on events.
    An events needs either a task or be
    available. Not both and not neither.
    It adds proper testing on events as well.
    
    * Add some logic validation on Events
    
    This commit adds validation on events.
    An events needs either a task or be
    available. Not both and not neither.
    It adds proper testing on events as well.

--- easy-planif/cal/models.py
+++ easy-planif/cal/models.py
@@ -1,9 +1,27 @@
 from accounts.models import CustomUser
+from django.core.exceptions import ValidationError
 from django.db import models
-from tasks.models import Tasks
+from tasks.models import Tasks, Authorizations
 
 class Event(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     tasks = models.ForeignKey(Tasks, on_delete=models.CASCADE, blank=True, null=True)
     is_available = models.BooleanField(default=True)
     date = models.DateTimeField()
+
+    def clean(self):
+        super().clean()
+
+        if self.tasks is not None and self.is_available:
+            raise ValidationError({"content": "Event cannot have a task and be available"})
+
+        if not self.is_available:
+            if self.tasks is None:
+                raise ValidationError({"content": "Event must have a task is not available"})
+            auth = Authorizations.objects.filter(user=self.user, task=self.tasks)
+            if len(auth) == 0:
+                raise ValidationError({"content": "Unauthorized"})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
